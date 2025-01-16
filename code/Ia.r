78f03e02e52c207ca79afdb94ca763358aa51905
raysas <rayane.s.adam@gmail.com>
2025-01-16 14:02:52 +0100

    -- added code

--- code/Ia.r
+++ code/Ia.r
@@ -8,8 +8,8 @@ fam<-read.table('I.a.Paramlink/fam.txt')
 colnames(fam)<-cols
 head(fam)
 
-cols<-c('Family_number','Individual_number','Father_number','Mother_number', 'Sex', 'Disease_status','maerke1_1','maerke1_2','maerke2_1','maerke2_2','maerke3_1','maerke3_2','maerke4_1','maerke4_2','maerke5_1','maerke5_2','maerke6_1','maerke6_2','maerke7_1','maerke7_2','maerke8_1','maerke8_2','maerke9_1','maerke9_2','maerke10_1','maerke10_2','maerke11_1','maerke11_2','maerke12_1','maerke12_2','maerke13_1','maerke13_2')
+cols<-c('Family_number','Individual_number','Father_number','Mother_number', 'Sex', 'Disease_status','marker1_1','marker1_2','marker2_1','marker2_2','marker3_1','marker3_2','marker4_1','marker4_2','marker5_1','marker5_2','marker6_1','marker6_2','marker7_1','marker7_2','marker8_1','marker8_2','marker9_1','marker9_2','marker10_1','marker10_2','marker11_1','marker11_2','marker12_1','marker12_2','marker13_1','marker13_2')
 
 fam<-read.table('I.a.Paramlink/fam.txt')
 colnames(fam)<-cols
-head(fam)
+fam[1:5,1:10]
